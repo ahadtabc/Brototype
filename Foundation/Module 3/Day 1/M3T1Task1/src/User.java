c49c79bfd34d9ca6a51c7fce3054da8f9f5131e2
Abdul Ahad <ahadtabc@gmail.com>
2026-05-05 22:21:28 +0530

    Update User.java

--- Foundation/Module 3/Day 1/M3T1Task1/src/User.java
+++ Foundation/Module 3/Day 1/M3T1Task1/src/User.java
@@ -1,28 +1,27 @@
 
 public class User {
-    // Properties (Instance Variables)
-    String name;
-    int age;
-    String place;
-    String email;
-    String phoneNumber;
-
-    // Constructor to initialize values
-    User(String name, int age, String place, String email, String phoneNumber) {
-        this.name = name;
-        this.age = age;
-        this.place = place;
-        this.email = email;
-        this.phoneNumber = phoneNumber;
-    }
-
-    // Method to display user details
-    void displayDetails() {
-        System.out.println("User Details:");
-        System.out.println("Name: " + name);
-        System.out.println("Age: " + age);
-        System.out.println("Place: " + place);
-        System.out.println("Email: " + email);
-        System.out.println("Phone Number: " + phoneNumber);
-    }
+	String name;
+	int age;
+	String place;
+	String email;
+	long phoneNumber;
+	
+	//name, age, place, email, phoneNumber 
+	
+	User(String name, int age, String place, String email, long phoneNumber){
+		this.name=name;
+		this.age=age;
+		this.place=place;
+		this.email=email;
+		this.phoneNumber=phoneNumber;
+	}
+	
+	void displayDetails() {
+		System.out.println("Name: "+name);
+		System.out.println("Age: "+age);
+		System.out.println("Place: "+place);
+		System.out.println("Email: "+email);
+		System.out.println("Phone Number: "+phoneNumber);
+	}
 }
+
