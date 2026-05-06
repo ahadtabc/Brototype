8d263c3cd68fb6de99b4997a729fef48db5237da
Abdul Ahad <ahadtabc@gmail.com>
2026-05-06 09:32:59 +0530

    Refactor Main class to use user input

--- Foundation/Module 3/Day 1/M3T1Task1/src/Main.java
+++ Foundation/Module 3/Day 1/M3T1Task1/src/Main.java
@@ -1,9 +1,43 @@
+// public class Main {
+	
+// 	public static void main(String[] args) {
+// 		User uc=new User(
+// 				"Abdul Ahad",29,"Malappuram","ahadtabc@gmail.com",
+// 				9074243208L);
+// 		uc.displayDetails();
+// 	}
+// }
+
+import java.util.Scanner;
+
 public class Main {
 	
 	public static void main(String[] args) {
-		User uc=new User(
-				"Abdul Ahad",29,"Malappuram","ahadtabc@gmail.com",
-				9074243208L);
-		uc.displayDetails();
+		
+		Scanner sc= new Scanner (System.in);
+		
+		System.out.println("Enter the Name :");
+		String name=sc.nextLine();
+		
+		System.out.println("Enter the Age :");
+		int age=sc.nextInt();
+		sc.nextLine();
+		
+		System.out.println("Enter the Place :");
+		String place=sc.nextLine();
+		
+		System.out.println("Enter the Email :");
+		String email=sc.nextLine();
+		
+		System.out.println("Enter the Phone Number :");
+		long phoneNumber=sc.nextLong();
+		
+		
+		User user1=new User(name, age, place, email, phoneNumber);
+				
+		System.out.println("----------------------");
+		user1.displayDetails();
+		sc.close();
 	}
+
 }
