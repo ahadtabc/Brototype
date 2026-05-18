09cc85fe0fe451421670a29b524d335db60f8a08
Abdul Ahad <ahadtabc@gmail.com>
2026-05-18 15:14:24 +0530

    Fix unique element count logic in main.c

--- Foundation/Module 4/Day 3/Assign 1.15/main.c
+++ Foundation/Module 4/Day 3/Assign 1.15/main.c
@@ -6,7 +6,7 @@ Array: {4, 5, 4, 6, 6, 7, 8}.
 
 The Array is : 4 5 4 6 6 7 8 
 Sorted Array is : 4 4 5 6 6 7 8 
-Number of unique elements are: 5 
+Number of unique elements are: 3 
 *******************************************************************************/
 #include <stdio.h>
 
@@ -14,7 +14,8 @@ int main()
 {
     int arr[]={4, 5, 4, 6, 6, 7, 8};
     int n=sizeof(arr)/sizeof(arr[0]);
-    int i,j,temp,count=0;
+    int i,j,temp;
+    int uniquecount=0,isduplicate;
     
     printf("The Array is : ");
     for(i=0; i<n; i++){
@@ -36,13 +37,15 @@ int main()
         printf("%d ",arr[i]);
     }
          for(i=0; i<n; i++){
-             if(arr[i]!=arr[i-1]){
-                     count++;
-                
-                 
-             }
+            isduplicate=0;
+             for(j=0; j<n; j++){
+                 isduplicate=1;
+                 break;
          }
-        printf("\nNumber of unique elements are: %d ",count);
+             if(isduplicate==0){
+                uniquecount++;
+         }
+        printf("\nNumber of unique elements are: %d ",uniquecount);
     
     
     return 0;
